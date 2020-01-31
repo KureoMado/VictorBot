b65dc26dd33f9ddc139ac98525dad1b0f61d0dba
MedivhDSbot <60485616+MedivhDSbot@users.noreply.github.com>
2020-01-31 18:18:51 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,8 +5,13 @@ from discord.ext.commands import Bot
 import requests
 import time
 import random
-import os
 from bs4 import BeautifulSoup as BS
+
+#PARSER
+
+#avi = av.select('img')
+#ava = avi[0]['src']
+
 #BOTS COMMANDS
 
 Bot = commands.Bot(command_prefix='v!')
@@ -15,7 +20,7 @@ Bot = commands.Bot(command_prefix='v!')
 @Bot.event
 async def on_ready():
     print("Neko bot is online")
-    
+
 @Bot.event
 async def on_message(ctx):
     channel = ctx.channel
@@ -25,7 +30,6 @@ async def on_message(ctx):
             await Bot.send_message(channel, "<:PuckHmm:672534849776779302>")
     await Bot.process_commands(ctx)
 
-#Testment
 @Bot.command(pass_context = True)
 async def test(ctx):
     await ctx.send("Test passed")
@@ -41,8 +45,8 @@ async def av(ctx):
 
     av = html.select('.avatar > img.my')[0]['src']
     av_url = "https://dota2.ru" + str(av)
-    await ctx.send(av_url)
-    await ctx.send(usrpg)
+    await Bot.say(av_url)
+    await Bot.say(usrpg)
 
 
 #Embed IMG
@@ -51,11 +55,8 @@ async def img(ctx):
     testm = "check"
     emb = discord.Embed(title="testm", colour=0x39d0d6)
     emb.set_image(url="https://dota2.ru/img/forum/avatars/l/638/638867.jpg")
-    await ctx.send(embed = emb)
-   
-@Bot.command(pass_context = True)
-async def cmds(ctx):
-    await ctx.send("Тут есть: test, img, av")
+    await Bot.say(embed = emb)
+
 
 token = os.environ.get('BOT_TOKEN')
 Bot.run(str(token))
