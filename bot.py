53c509124afed4eb2bf5c0023b7d06601c208fff
MedivhDSbot <60485616+MedivhDSbot@users.noreply.github.com>
2020-08-01 20:25:29 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -19,6 +19,13 @@ now = datetime.now()
 Bot = commands.Bot(command_prefix=config.PREFIX)
 Bot.remove_command("help")
 
+#Консольное уведомление о запуске бота
+@Bot.event
+async def on_ready():
+    print("Mengsk bot is online")
+    channel = Bot.get_channel(739121375016386682)
+    await channel.send('Пий XIII был запущен')
+
 @Bot.event
 async def on_command_error(self, error):
     channel = self.channel
@@ -47,24 +54,8 @@ async def on_raw_reaction_add(ctx):
 @Bot.command()
 async def help(ctx):
         emb = discord.Embed(title='Пий XIII', colour=0x33ccff) #Текст выводится с помощью метода Embed
-        emb.add_field(name='Информация:', value="\nВерсия: 0.9.9a\n\nВот что я могу:\n\npat @пользователь - погладить юзера <:pat2:672538535156252672>\ncovid - статистика по COVID-19 <:durka:672538535235944488>\nmoder и osnova - <:DankPepe:675661963640045569>")
+        emb.add_field(name='Информация:', value="\nВерсия: 0.9.9c\n\nВот что я могу:\n\npat @пользователь - погладить юзера <:pat2:672538535156252672>\ncovid - статистика по COVID-19 <:durka:672538535235944488>\nmoder и osnova - <:DankPepe:675661963640045569>")
         await ctx.send(embed = emb)
-
-#PAT
-@Bot.command()
-async def pat(ctx, member: discord.Member):
-    #EMBED метод
-    title = str(ctx.author.display_name) + " гладит " + str(member.display_name) + " <:pat2:672538535156252672>" #Название Embed элемента
-    emb = discord.Embed(title=title, colour=imglist.CLR_LIST[random.randint(0, imglist.CLR_LIST_LEN)])
-    emb.set_image(url=imglist.PAT_LIST[random.randint(0, imglist.PAT_LIST_LEN)])
-    await ctx.send(embed = emb)
-
-#covid
-@Bot.command()
-@commands.cooldown(1, 60, commands.BucketType.guild)
-async def covid(ctx):
-    covid = apps.covid()
-    await ctx.send('Статистика по COVID-19 (З / У / В):\n\nМир: {0} / {1} / {2}\nРоссия: {3} / {4} / {5}\nУкраина: {6} / {7} / {8}\nБеларусь: {9} / {10} / {11}\nКазахстан: {12} / {13} / {14}'.format(*covid))
 
 #MODER
 @Bot.command()
@@ -72,8 +63,7 @@ async def covid(ctx):
 @commands.has_permissions(administrator = True) #Команду могут использовать только администраторы сервера
 async def moder(ctx):
     tick = datetime.now() #TIMER START
-    d2ru_category = 'Разное'
-    links = apps.d2ru_violations(str(d2ru_category)) #получение списка постов с нарушениями. Функция описана в apps.py
+    links = apps.d2ru_violations('Разное') #получение списка постов с нарушениями. Функция описана в apps.py
     tock = datetime.now()
     diff = tock - tick
     if len(links) != 0: #Проверка на отсутствие нарушений
@@ -91,8 +81,7 @@ async def moder(ctx):
 @commands.has_permissions(administrator = True) #Команду могут использовать только администраторы сервера
 async def osnova(ctx):
     tick = datetime.now() #TIMER START
-    d2ru_category = 'Основа'
-    links = apps.d2ru_violations(str(d2ru_category)) #получение списка постов с нарушениями. Функция описана в apps.py
+    links = apps.d2ru_violations('Основа') #получение списка постов с нарушениями. Функция описана в apps.py
     tock = datetime.now()
     diff = tock - tick
     if len(links) != 0: #Проверка на отсутствие нарушений
@@ -104,6 +93,69 @@ async def osnova(ctx):
         await ctx.send('На этом все <:MiyanoYey:672534850066055191>')
     else:
         await ctx.send('Результат поиска по **основному** разделу:\nНарушения не найдены <:MiyanoYey:672534850066055191> \nПоиск занял ' + str(int(diff.total_seconds())) + ' сек.')
+
+#поиск в техе
+@Bot.command()
+@commands.cooldown(1, 1800, commands.BucketType.user) #Кд в 30 минут
+@commands.has_permissions(administrator = True) #Команду могут использовать только администраторы сервера
+async def tech(ctx):
+    tick = datetime.now() #TIMER START
+    links = apps.d2ru_violations('Тех') #получение списка постов с нарушениями. Функция описана в apps.py
+    tock = datetime.now()
+    diff = tock - tick
+    if len(links) != 0: #Проверка на отсутствие нарушений
+        await ctx.send('Результат поиска в **техническом разделе**:\nНайдено нарушений: ' + str(len(links)) + '\nПоиск занял ' + str(int(diff.total_seconds())) + ' сек.')
+        for i in range(len(links)):
+            await ctx.send(links[i])
+            time.sleep(0.5)
+        time.sleep(0.5)
+        await ctx.send('На этом все <:MiyanoYey:672534850066055191>')
+    else:
+        await ctx.send('Результат поиска в **других играх и разном**:\nНарушения не найдены <:MiyanoYey:672534850066055191> \nПоиск занял ' + str(int(diff.total_seconds())) + ' сек.')
+
+#super
+@Bot.command()
+@commands.cooldown(1, 1800, commands.BucketType.user) #Кд в 30 минут
+@commands.has_permissions(administrator = True) #Команду могут использовать только администраторы сервера
+async def super(ctx):
+    tick = datetime.now() #TIMER START
+    links =  apps.super_violation() #получение списка постов с нарушениями. Функция описана в apps.py
+    tock = datetime.now()
+    diff = tock - tick
+    if len(links[0]) != 0: #Проверка на отсутствие нарушений
+        await ctx.send('Результат поиска в **других играх и разном**:\nНайдено нарушений: ' + str(len(links[0])))
+        for i in range(len(links[0])):
+            await ctx.send(links[0][i])
+            time.sleep(0.5)
+        time.sleep(1)
+    else:
+        await ctx.send('В **других играх и разном** ничего не найдено <:ChildPepeCry:672538534493814825>')
+        time.sleep(1)
+
+    await ctx.send('<:Addsky:672537907680247828><:Addsky:672537907680247828><:Addsky:672537907680247828> ')
+
+    if len(links[1]) != 0: #Проверка на отсутствие нарушений
+        await ctx.send('Результат поиска в **основном разделе**:\nНайдено нарушений: ' + str(len(links[1])))
+        for i in range(len(links[1])):
+            await ctx.send(links[1][i])
+            time.sleep(0.5)
+        time.sleep(1)
+    else:
+        await ctx.send('В **оснонвом разделе** ничего не найдено <:ChildPepeCry:672538534493814825>')
+        time.sleep(1)
+
+    await ctx.send('<:Addsky:672537907680247828><:Addsky:672537907680247828><:Addsky:672537907680247828> ')
+
+    if len(links[2]) != 0: #Проверка на отсутствие нарушений
+        await ctx.send('Результат поиска в **техническом**:\nНайдено нарушений: ' + str(len(links[2])))
+        for i in range(len(links[2])):
+            await ctx.send(links[2][i])
+            time.sleep(0.5)
+        time.sleep(1)
+    else:
+        await ctx.send('В **техническом** ничего не найдено <:ChildPepeCry:672538534493814825>')
+        time.sleep(1)
+    await ctx.send('На этом все <:MiyanoYey:672534850066055191>\nПоиск занял ' + str(int(diff.total_seconds())) + ' сек.')
 
 #END
 tokenr = config.TOKEN
